d40e37405dfed1f9575deaa4c1e2a2eac93bf3cd
weykon <635883092@qq.com>
2022-03-19 09:01:41 +0800

    条件渲染

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Comment, mock_comment } from './component/Comment'
-import { Clock } from "./component/Clock"
-import Toggle from "./component/Toggle"
+import React from "react";
+import ReactDOM from "react-dom";
+import { Comment, mock_comment } from "./component/Comment";
+import { Clock } from "./component/Clock";
+import Toggle from "./component/Toggle";
+import { Greeting } from "./component/Greeting";
 function Hello(props: any) {
-    return (
-        <div>
-            <h1>Hello</h1>
-            <h1>    {props.name}</h1>
-        </div>
-
-    )
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>{props.name}</h2>
+    </div>
+  );
 }
 
 function App() {
-    return (
-        <div>
-            <Hello name='wykon'></Hello>
-            <Hello name='abc'></Hello>
-            <Hello name='weykon'></Hello>
-            <Comment date={mock_comment.date} text={mock_comment.text} author={mock_comment.author} ></Comment>
-            <Clock date={new Date()}></Clock>
-            <Toggle/>
-        </div >
-    )
+  return (
+    <div>
+      <Hello name="wykon"></Hello>
+      <Hello name="abc"></Hello>
+      <Hello name="weykon"></Hello>
+      <Comment
+        date={mock_comment.date}
+        text={mock_comment.text}
+        author={mock_comment.author}
+      ></Comment>
+      <Clock date={new Date()}></Clock>
+      <Toggle />
+      <Greeting isLoggedIn={false} />
+    </div>
+  );
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("app")
-)
-
-
+ReactDOM.render(<App />, document.getElementById("app"));
